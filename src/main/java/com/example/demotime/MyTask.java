31c958e38f462572641d348c21939ced55ccb339
EKAvdeev <19215200@sigma.sbrf.ru>
2021-08-10 01:53:57 +0500

    sportaf

--- src/main/java/com/example/demotime/MyTask.java
+++ src/main/java/com/example/demotime/MyTask.java
@@ -2,6 +2,8 @@ package com.example.demotime;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -21,7 +23,11 @@ public class MyTask {
 
     @Scheduled(fixedRate = 60000)
     public void CurrentTime() throws Exception {
-        doc = Jsoup.connect("https://www.sports.ru/fantasy/").get();
+//        System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.marathonbet.ru/su/popular/Football/France/Ligue+1+-+21533");
+//        Document doc = Jsoup.parse(driver.getPageSource());
+        doc = Jsoup.connect("https://www.marathonbet.ru/su/popular/Football/France/Ligue+1+-+21533").get();
         times = dtf.format(LocalDateTime.now(ZoneId.of("Europe/Moscow")));
         System.out.println("Now: " + times);
     }
